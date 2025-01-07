5b1f9b6760183c72b537e8650e32f9ee66c13de6
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2025-01-07 01:03:03 -0400

    Configure Swagger, API versioning, and JWT security definitions

---
+++ Restaurant.WebApi/Extensions/AppExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant.WebApi.Extensions
+{
+    public static class AppExtensions
+    {
+        public static void UseSwaggerExtension(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API"); // Ruta que queremos que tengan por defecto todas nuestras configuraciones de Swagger y nombre de nuestra API.
+            });
+        }
+    }
+}
---
+++ Restaurant.WebApi/Extensions/ServiceExtension.cs
@@ -0,0 +1,79 @@
+﻿using Asp.Versioning;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using RestaurantWebApi.Core.Application.Enums;
+
+namespace Restaurant.WebApi.Extensions
+{
+    public static class ServiceExtension
+    {   
+        public static void AddSwaggerExtension(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                List<string> xmlFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.xml", searchOption: SearchOption.TopDirectoryOnly).ToList();
+                xmlFiles.ForEach(xmlFile => options.IncludeXmlComments(xmlFile));
+
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Restaurant API",
+                    Description = "This api will be responsible for overall data distribution",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Federico A. Garcia",
+                        Email = "fagarciatavarez@gmail.com",
+                        Url = new Uri("https://github.com/AleGxrcia")
+                    }
+                });
+
+                options.DescribeAllParametersInCamelCase();
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization", 
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "Input your Bearer token in this format - Bearer {your token here}",
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "Bearer",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        }, new List<string>()
+                    },
+                });
+
+                options.MapType<Status>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    Enum = Enum.GetNames(typeof(Status))
+                                .Select(name => new OpenApiString(name))
+                                .ToList<IOpenApiAny>()
+                });
+
+            });
+        }
+
+        public static void AddApiVersioningExtension(this IServiceCollection services)
+        {
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+            });
+        }
+    }
+}
